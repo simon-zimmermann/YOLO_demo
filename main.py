1a5e5b4f771e996a68fa0924c1da36bede0de6df
simon-zimmermann <132376719+simon-zimmermann@users.noreply.github.com>
2024-05-16 11:49:07 +0200

    improvements

--- main.py
+++ main.py
@@ -2,69 +2,88 @@ import cv2
 from ultralytics import YOLO
 from ultralytics.utils.plotting import Annotator, colors
 import time
-import numpy as np
 
 model = YOLO("yolov8n-seg.pt")  # segmentation model, see https://docs.ultralytics.com/tasks/segment/#models
-names = model.model.names
-cap = cv2.VideoCapture(0)
-w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+names = model.model.names  # use predefined class names
 
-scaled_height = 800
-scaled_width = (w / h) * capture_height
+# Read background image
+bg = cv2.imread('YOLO demonstrator BG.png')
 
-bg = cv2.imread('ml_demo.jpg')
-
+# Create window in fullscreen mode
 cv2.namedWindow('ML_DEMO', cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty('ML_DEMO', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-#scaling_factor = 800 / 416
+(win_x, win_y, win_w, win_h) = cv2.getWindowImageRect('ML_DEMO')
+print("Window size: ", win_w, win_h)
 
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # Get capture device
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 10000)  # try to set some insane resolution
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 10000)
+# get actual maximum resolution
+capture_w, capture_h = (int(cap.get(x))
+                        for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT))
+print("Capture size: ", capture_w, capture_h)
+
+# Calculate scaling for captured stream
+# scale the webcam stream to a factor of the window height
+# Factor determined by trial and error
+scaled_height = int(0.755 * win_h)
+scaled_width = int((capture_w / capture_h) * scaled_height)
+print("Scaled size: ", scaled_width, scaled_height)
+
+# Calculate start position for the scaled stream on the background image
+start_y = int((win_h - scaled_height) / 2)
+start_x = int((win_w - scaled_width) / 2)
+print("Start position: ", start_x, start_y)
 
 while True:
-    (grabbed, frame) = cap.read()
+    (grabbed, frame_cap) = cap.read()
     if not grabbed:
         exit()
-    frame = cv2.flip(frame, 1)
+    frame_cap = cv2.flip(frame_cap, 1)
 
-    start = time.time()
-    results = model.predict(frame)
-    end = time.time()
+    # AI inference, measure time, see https://docs.ultralytics.com/tasks/segment/#inference
+    start_inference = time.time()
+    results = model.predict(frame_cap)
+    end_inference = time.time()
 
+    # Draw stream, bg image and fps
     start_drawing = time.time()
-    annotator = Annotator(frame, line_width=2)
-
+    annotator = Annotator(frame_cap, line_width=2)
     if results[0].masks is not None:
-        clss = results[0].boxes.cls.cpu().tolist()
+        cls = results[0].boxes.cls.cpu().tolist()
         masks = results[0].masks.xy
-        for mask, cls in zip(masks, clss):
+        for mask, cls in zip(masks, cls):
             annotator.seg_bbox(mask=mask,
                                mask_color=colors(int(cls), True),
                                det_label=names[int(cls)])
+
+    # Resize stream to fit window
+    resized = cv2.resize(frame_cap, (scaled_width, scaled_height), interpolation=cv2.INTER_AREA)
+    # Create display frame, start with background image
+    frame = cv2.resize(bg, (win_w, win_h), interpolation=cv2.INTER_AREA)
+    # Add scaled stream to background image
+    frame[start_y:start_y + scaled_height, start_x:start_x + scaled_width] = resized
+
     end_drawing = time.time()
-
-    # fps_label = "FPS: %.2f (excluding drawing time of %.2fms)" % (1 / (end - start), (end_drawing - start_drawing) * 1000)
-    # cv2.putText(frame, fps_label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
-
-    #resized = cv2.resize(frame, (resized_w, resized_h), interpolation=cv2.INTER_AREA)
-    #blank_image = np.zeros((1080, 1920, 3), np.uint8)
-
-#    frame = bg
-
-    # height=1080
-    # width = 1920
- #   start_y = int((h - resized_h) / 2)
-  #  start_x = int((w - resized_w) / 2)
-   # frame[start_y:start_y+resized_h, start_x:start_x+resized_w] = resized
-
-    # frame[110:910, (width - 800)//2:((width-800)//2)+800] = frame
-
-    #msg = "Zentrum Industrie 4.0 ML Demo"
-   # cv2.putText(frame, msg, (((width)//2)-8*len(msg), 950), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    # Add FPS to display frame
+    inference_time = (end_inference - start_inference) * 1000
+    fps = 1 / (end_drawing - start_inference)  # overall FPS
+    drawing_time = (end_drawing - start_drawing) * 1000
+    fps_label = "FPS: %.2f (inference: %.2fms, drawing: %.2fms)" % (fps, inference_time, drawing_time)
+    cv2.putText(img=frame,
+                text=fps_label,
+                org=(start_x + 5, start_y + 19),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=0.6,
+                color=(0, 0, 0),
+                thickness=1,
+                lineType=cv2.LINE_AA)
 
     cv2.imshow("ML_DEMO", frame)
 
+    # to quit, press 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# out.release()
 cap.release()
 cv2.destroyAllWindows()
