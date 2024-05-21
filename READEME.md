70d223a3840cf0b9d8c187f2f6acc16b31851922
simon-zimmermann <132376719+simon-zimmermann@users.noreply.github.com>
2024-05-21 14:55:18 +0200

    integrated a version using YOLOv7

--- READEME.md
+++ READEME.md
@@ -1,4 +1,18 @@
-# Install
+# Live demo of YOLO object detection using a webcam
+
+There are two versions available: YOLOv7-tiny and YOLOv8.
+
+YOLOv7-tiny is a smaller and more optimized/compressed version of YOLOv7, and can be used on very low-performance devices. However, its accuracy is quite poor.
+
+YOLOv8 is a more accurate (and more recent) version of YOLO, but it requires a CUDA compatible GPU to run at resonable performance.
+
+## Requirements
+
+- Python 3.10 or later
+- A CUDA compatible GPU (only for the yolov8.py)
+- A webcam accessible by OpenCV
+
+## Install
 
 Make sure to have python 3 installed.
 Create a virtual environment and activate it.
@@ -9,9 +23,9 @@ source .venv/bin/activate
 ```
 
 Then install the following packages using pip:
- - opencv-python
- - ultralytics
+Install torch first to use the correct CUDA package.
 
 ```bash
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 pip install opencv-python ultralytics
 ```
---
+++ yolov8.py
@@ -0,0 +1,103 @@
+import cv2
+from ultralytics import YOLO
+from ultralytics.utils.plotting import Annotator, colors
+import time
+import torch
+
+# Make sure to actually use the GPU for inference
+print("Using torch version: ", torch.__version__)
+print("CUDA Support: ", torch.cuda.is_available())
+torch.cuda.set_device(0)  # use the GPU for inference
+
+model = YOLO("yolov8x-seg.pt")  # segmentation model, see https://docs.ultralytics.com/tasks/segment/#models
+names = model.model.names  # use predefined class names
+
+# Read background image
+bg = cv2.imread("yolov8_BG.png")
+
+# Create window in fullscreen mode
+cv2.namedWindow('ML_DEMO', cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty('ML_DEMO', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+(win_x, win_y, win_w, win_h) = cv2.getWindowImageRect('ML_DEMO')
+print("Window size: ", win_w, win_h)
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # Get capture device
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 10000)  # try to set some insane resolution
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 10000)
+# get actual maximum resolution
+capture_w, capture_h = (int(cap.get(x))
+                        for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT))
+print("Capture size: ", capture_w, capture_h)
+
+# Calculate scaling for captured stream
+# scale the webcam stream to a factor of the window height
+# Factor determined by trial and error
+scaled_height = int(0.755 * win_h)
+scaled_width = int((capture_w / capture_h) * scaled_height)
+print("Scaled size: ", scaled_width, scaled_height)
+
+# Calculate start position for the scaled stream on the background image
+start_y = int((win_h - scaled_height) / 2)
+start_x = int((win_w - scaled_width) / 2)
+print("Start position: ", start_x, start_y)
+
+while True:
+    (grabbed, frame_cap) = cap.read()
+    if not grabbed:
+        exit()
+    frame_cap = cv2.flip(frame_cap, 1)
+
+    # AI inference, measure time, see https://docs.ultralytics.com/tasks/segment/#inference
+    start_inference = time.time()
+    results = model.predict(frame_cap)
+    end_inference = time.time()
+
+    # Draw stream, bg image and fps
+    start_drawing = time.time()
+    annotator = Annotator(frame_cap, line_width=2, pil=False)
+    if results[0].masks is not None:
+        # Classes of detected objects
+        det_classes = results[0].boxes.cls.cpu().tolist()
+        # Masks of detected objects
+        det_masks = results[0].masks.xy
+        # Confidence of detected objects
+        det_confidence = results[0].boxes.conf.cpu().tolist()
+
+        # Draw bounding boxes and labels for each detected object
+        for mask, cls, confid in zip(det_masks, det_classes, det_confidence):
+            label_str = "%s: %d%%" % (names[int(cls)], confid * 100)
+            # Annotate image
+            annotator.seg_bbox(mask=mask,
+                               mask_color=colors(int(cls), True),
+                               det_label=label_str)
+
+    # Create display frame, start with background image
+    frame = cv2.resize(bg, (win_w, win_h), interpolation=cv2.INTER_AREA)
+    # Resize stream to fit window
+    resized = cv2.resize(frame_cap, (scaled_width, scaled_height), interpolation=cv2.INTER_AREA)
+    # Add scaled stream to background image
+    frame[start_y:start_y + scaled_height, start_x:start_x + scaled_width] = resized
+
+    end_drawing = time.time()
+    # Add FPS to display frame
+    inference_time = (end_inference - start_inference) * 1000
+    fps = 1 / (end_drawing - start_inference)  # overall FPS
+    drawing_time = (end_drawing - start_drawing) * 1000
+    fps_label = "FPS: %.2f (inference: %.2fms, drawing: %.2fms)" % (fps, inference_time, drawing_time)
+    cv2.putText(img=frame,
+                text=fps_label,
+                org=(start_x + 5, start_y + 19),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=0.6,
+                color=(0, 0, 0),
+                thickness=1,
+                lineType=cv2.LINE_AA)
+
+    cv2.imshow("ML_DEMO", frame)
+
+    # to quit, press 'q'
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
